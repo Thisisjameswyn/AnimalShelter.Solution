c96128427abb1bbf217ceefc297ff1d906b7a499
James Wyn <thisisjameswyn@gmail.com>
2021-06-14 07:24:10 -0700

    add additional api framework, controller and seed data

---
+++ AnimalShelter/Migrations/20210614140905_Initial.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    AnimalId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(20) CHARACTER SET utf8mb4", maxLength: 20, nullable: false),
+                    Species = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Gender = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.AnimalId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Gender", "Name", "Species" },
+                values: new object[] { 1, 1, "Test Gender", "Test Name", "Test Species" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animals");
+        }
+    }
+}
--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -12,13 +12,17 @@ namespace AnimalShelter.Models
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
-      builder.Entity<Group>()
+      builder.Entity<Animal>()
         .HasData(
 
-          new Animal { AnimalId = 1, Name = "Test Name", Species = "Test Species", Age = 1, Gender = "Test Gender" }
+          new Animal { AnimalId = 1, Name = "Test Name", Species = "Test Species", Age = 1, Gender = "Test Gender" },
+          new Animal { AnimalId = 2, Name = "Vivian", Species = "Cat", Age = 12, Gender = "Female" },
+          new Animal { AnimalId = 3, Name = "Claire", Species = "Dog", Age = 4, Gender = "Female Gender" },
+          new Animal { AnimalId = 4, Name = "Marshal", Species = "Cat", Age = 1, Gender = "Male" },
+          new Animal { AnimalId = 5, Name = "Bob", Species = "Chicken", Age = 1, Gender = "Female" },
+          new Animal { AnimalId = 6, Name = "Not Bob", Species = "Chicken", Age = 1, Gender = "Female" }
         );
     }
-    public DbSet<Group> Groups { get; set; }
-    public DbSet<Message> Messages { get; set; }
+    public DbSet<Animal> Animals { get; set; }
   }
 }
